520ffec37ae679d6c216830a3aa234899941c54a
Kalioz <Kalioz@172.21.1.7>
2017-02-23 20:29:43 +0100

    g

--- Hashcode/src/Cache.java
+++ Hashcode/src/Cache.java
@@ -2,7 +2,16 @@
 public class Cache {
 	int UID;
 	static int nbCreated;
-		
+	int [][] video;
+	
+	public int[][] getVideo() {
+		return video;
+	}
+
+	public void setVideo(int[][] video) {
+		this.video = video;
+	}
+
 	Cache(){
 		this.UID = Cache.nbCreated;
 		Cache.nbCreated++;
