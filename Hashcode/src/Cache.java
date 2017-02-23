7bf4cb05ec2f74f6acec4a39a38af7101ad25c07
Florent <florent.migne@reseau.eseo.fr>
2017-02-23 20:58:25 +0100

    pushe

--- Hashcode/src/Cache.java
+++ Hashcode/src/Cache.java
@@ -2,21 +2,57 @@
 public class Cache {
 	int UID;
 	static int nbCreated;
-	int [][] video;
+	int [][] listevideo;
+	int [] cacheVideo;
+	int [] listeEndPoints;
+	//getters and setters
+	public int[] getlisteEndPoints() {
+		return this.listeEndPoints;
+	}
+
+	public void setlisteEndPoints(int[] listeEP) {
+		this.listeEndPoints = listeEP;
+	}
 	
-	public int[][] getVideo() {
-		return video;
+	
+	public int[][] getListeVideo() {
+		return listevideo;
 	}
 
-	public void setVideo(int[][] video) {
-		this.video = video;
+	public void setListeVideo(int[][] video) {
+		this.listevideo = video;
+	}
+	public int[] getCacheVideo() {
+		return this.cacheVideo;
 	}
 
+	public void setCacheVideo(int[] cache) {
+		this.cacheVideo = cache;
+	}
+	
+	//constructeur
 	Cache(){
 		this.UID = Cache.nbCreated;
 		Cache.nbCreated++;
+		
 	}
 	
 	int getUID(){return this.UID;}
 	
+	boolean possedeVideo(int numeroVideo){
+		boolean retour = false;
+		for (int i=0;i<this.getListeVideo().length;i++){
+			if(this.getListeVideo()[i][0]==numeroVideo){
+				retour = true;
+			}
+		}
+		return retour;
+	}
+	void construitRapport(){
+		
+	}
+	
+	void classeVideos(){
+		
+	}
 }
